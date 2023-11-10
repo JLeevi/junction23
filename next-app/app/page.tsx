7393d7c72a0a66a8c5df48eaa73488edf9615762
alekshiidenhovi <76514868+alekshiidenhovi@users.noreply.github.com>
2023-11-11 01:11:37 +0200

    globe visual improvements

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -14,28 +14,31 @@ export default function Home() {
     let phi = 0
 
     const globe = createGlobe(canvasRef.current, {
-      devicePixelRatio: 2,
-      width: 600 * 2,
-      height: 600 * 2,
       phi: 0,
       theta: 0,
-      dark: 1,
-      diffuse: 1.2,
       mapSamples: 16000,
-      mapBrightness: 6,
-      baseColor: [0.3, 0.3, 0.3],
+      mapBrightness: 6.5,
+      mapBaseBrightness: 0.0,
+      diffuse: 1.2,
+      dark: 0,
+      baseColor: [1, 1, 1],
       markerColor: [0.1, 0.8, 1],
-      glowColor: [1, 1, 1],
       markers: [
         // longitude latitude
         { location: [37.7595, -122.4367], size: 0.03 },
         { location: [40.7128, -74.006], size: 0.1 },
       ],
+      scale: 1,
+      opacity: 0.85,
+      glowColor: [0.7, 0.7, 0.7],
+      devicePixelRatio: 2,
+      width: 600 * 2,
+      height: 600 * 2,
       onRender: (state) => {
         // Called on every animation frame.
         // `state` will be an empty object, return updated params.
         state.phi = phi
-        phi += 0.01
+        phi += 0.001
       },
     })
 
