62f7db8da63354424faa64211a46b0ca31066f6f
Leevi Jäkälä <jakala.leevi@gmail.com>
2023-11-11 20:47:49 +0200

    sh file to locally run pipeline

--- pipeline/data-pipeline.sh
+++ pipeline/data-pipeline.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-echo "Fetching news data..."
-poetry run python fetch-news-data.py
+source venv/bin/activate
 
-echo "Fetcing disaster data..."
-poetry run python fetch-disaster-data.py
+# Define color codes
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
 
-echo "Script completed."
+echo -e "${BLUE}Fetching news and disaster events...${NC}"
+python get_events.py
+echo -e "${GREEN}Done!${NC}"
+
+echo -e "${BLUE}Generating risk analysis...${NC}"
+python get_events.py
+echo -e "${GREEN}Done!${NC}"
+
+echo -e "${YELLOW}Pipeline completed${NC}"
