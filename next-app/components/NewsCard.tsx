7625287453642c6f5f7fcabf177bb3a92a7f75cf
Leevi Jäkälä <jakala.leevi@gmail.com>
2023-11-12 06:11:45 +0200

    make whole newsarticle a link

--- next-app/components/NewsCard.tsx
+++ next-app/components/NewsCard.tsx
@@ -10,18 +10,18 @@ type Props = NewsCardData & ComponentProps<"div">
 
 export const NewsCard = ({ summary, url, ...props }: Props) => {
   return (
-    <div
-      {...props}
-      className="flex flex-shrink-0 justify-between space-x-4 rounded-lg border-2 bg-white p-4"
-    >
-      <div className="space-y-1">
-        <p className="text-sm font-medium">{summary}</p>
-        <div className="flex items-center pt-2">
-          <Link href={url} className="text-xs text-muted-foreground">
+    <Link href={url}>
+      <div
+        {...props}
+        className="flex flex-shrink-0 justify-between space-x-4 rounded-lg border-2 bg-white p-4 transition-all hover:border-slate-300"
+      >
+        <div className="space-y-1">
+          <p className="text-sm font-medium">{summary}</p>
+          <div className="flex items-center pt-2 text-xs text-muted-foreground ">
             {url}
-          </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
